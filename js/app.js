7246dd0c20c00befd217f908eb8bca1d47253a1c
Christina <christinamarieelaine@gmail.com>
2026-01-07 20:22:27 -0800

    Steps 4 and 5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+const winningCombos = [
+  [0, 1, 2], // top row
+  [3, 4, 5], // middle row
+  [6, 7, 8], // bottom row
+  [0, 3, 6], // left column
+  [1, 4, 7], // middle column
+  [2, 5, 8], // right column
+  [0, 4, 8], // diagonal
+  [2, 4, 6], // diagonal
+];
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -30,6 +39,29 @@ function init() {
   tie = false;
 
   render();
+}
+
+function render() {
+  updateBoard();
+  updateMessage();
+}
+
+function updateBoard() {
+  board.forEach((cell, idx) => {
+    // idx matches the square id and the NodeList order
+    squareEls[idx].textContent = cell;
+  });
+}
+
+function updateMessage() {
+  if (winner === false && tie === false) {
+    messageEl.textContent = `It's ${turn}'s turn`;
+  } else if (winner === false && tie === true) {
+    messageEl.textContent = `It's a tie!`;
+  } else {
+    // winner is true, so the current turn is the winner
+    messageEl.textContent = `Congrats ${turn}! You won!`;
+  }
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
