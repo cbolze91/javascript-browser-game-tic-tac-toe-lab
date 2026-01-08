10bd67ce8e5be6578ce424e5e6190d57215d92c5
Christina <christinamarieelaine@gmail.com>
2026-01-07 19:56:08 -0800

    Step 2

--- js/app.js
+++ js/app.js
@@ -11,7 +11,11 @@ let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.querySelectorAll('.sqr');
+const messageEl = document.querySelector('#message');
 
+console.log(squareEls);
+console.log(messageEl);
 
 /*-------------------------------- Functions --------------------------------*/
 
